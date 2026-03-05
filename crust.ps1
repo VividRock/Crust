a5a4307333a90324f1f7312d23a42b94ff623701
Dustin Estes <85371705+dustinestes@users.noreply.github.com>
2026-03-04 22:08:16 -0600

    Feature: Decouple Language and Menu Structure Fixes #3

--- crust.ps1
+++ crust.ps1
@@ -6,6 +6,14 @@
 [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low', DefaultParameterSetName = "Default")]
 param(
   [Parameter(Mandatory = $false,
+    HelpMessage = "Specify the name of a menu json file.",
+    ParameterSetName = "Default")]
+  [string]$File = "menu.json",
+  [Parameter(Mandatory = $false,
+    HelpMessage = "Specify the name of a menu in the json file to load with Crust.",
+    ParameterSetName = "Default")]
+  [string]$Name = "Main",
+  [Parameter(Mandatory = $false,
     HelpMessage = "Specify an explicit language code if you want to override the user's preferred language.",
     ParameterSetName = "Language")]
     [ValidatePattern("[a-z]{2}-[A-Z]{2}")]
@@ -36,10 +44,10 @@ Set-Tokens
 
 Out-File -InputObject "  Show Main Menu" @Params_Logging
 
-$Menu = "Main"
 Clear-Interface
-while ($Menu -ne "Quit") {
-  $Menu = Show-InterfaceMenu -MenuName $Menu
+$Menu = Get-InterfaceMenu -File $File
+while ($Name -ne "Quit") {
+  $Name = Show-InterfaceMenu -Menu ($Menu | Where-Object -Property "Name" -eq $Name) -Name $Name
 }
 
 Out-File -InputObject "    - Complete" @Params_Logging
@@ -55,18 +63,18 @@ Out-File -InputObject " " @Params_Logging
 #Region Footer
 
 # Gather Data
-$Config_initialize.Metadata.ScriptResult = "Success"
-$Config_initialize.Metadata.ScriptResultCode = 0
-$Config_initialize.Metadata.CompleteDateTime = Get-Date
-$Config_initialize.Metadata.CompleteTimeSpan = New-TimeSpan -Start $Config_initialize.Metadata.StartDateTime -End $Config_initialize.Metadata.CompleteDateTime
+$Crust.Metadata.ScriptResult = "Success"
+$Crust.Metadata.ScriptResultCode = 0
+$Crust.Metadata.CompleteDateTime = Get-Date
+$Crust.Metadata.CompleteTimeSpan = New-TimeSpan -Start $Crust.Metadata.StartDateTime -End $Crust.Metadata.CompleteDateTime
 
 # Output
 Out-File -InputObject " " @Params_Logging
 Out-File -InputObject "------------------------------------------------------------------------------" @Params_Logging
-Out-File -InputObject "  Script Result: $($Config_initialize.Metadata.ScriptResult)" @Params_Logging
-Out-File -InputObject "  Script Started: $($Config_initialize.Metadata.StartDateTime.ToUniversalTime().ToString(`"yyyy-MM-dd HH:mm:ss`")) (UTC)" @Params_Logging
-Out-File -InputObject "  Script Completed: $($Config_initialize.Metadata.CompleteDateTime.ToUniversalTime().ToString(`"yyyy-MM-dd HH:mm:ss`")) (UTC)" @Params_Logging
-Out-File -InputObject "  Total Time: $($Config_initialize.Metadata.CompleteTimeSpan.Days) days, $($Config_initialize.Metadata.CompleteTimeSpan.Hours) hours, $($Config_initialize.Metadata.CompleteTimeSpan.Minutes) minutes, $($Config_initialize.Metadata.CompleteTimeSpan.Seconds) seconds, $($Config_initialize.Metadata.CompleteTimeSpan.Milliseconds) milliseconds" @Params_Logging
+Out-File -InputObject "  Script Result: $($Crust.Metadata.ScriptResult)" @Params_Logging
+Out-File -InputObject "  Script Started: $($Crust.Metadata.StartDateTime.ToUniversalTime().ToString(`"yyyy-MM-dd HH:mm:ss`")) (UTC)" @Params_Logging
+Out-File -InputObject "  Script Completed: $($Crust.Metadata.CompleteDateTime.ToUniversalTime().ToString(`"yyyy-MM-dd HH:mm:ss`")) (UTC)" @Params_Logging
+Out-File -InputObject "  Total Time: $($Crust.Metadata.CompleteTimeSpan.Days) days, $($Crust.Metadata.CompleteTimeSpan.Hours) hours, $($Crust.Metadata.CompleteTimeSpan.Minutes) minutes, $($Crust.Metadata.CompleteTimeSpan.Seconds) seconds, $($Crust.Metadata.CompleteTimeSpan.Milliseconds) milliseconds" @Params_Logging
 Out-File -InputObject "------------------------------------------------------------------------------" @Params_Logging
 Out-File -InputObject "  End of Script" @Params_Logging
 Out-File -InputObject "------------------------------------------------------------------------------" @Params_Logging
@@ -74,4 +82,4 @@ Out-File -InputObject "---------------------------------------------------------
 #EndRegion Footer
 #--------------------------------------------------------------------------------------------
 
-Return $Config_initialize.Metadata.ScriptResultCode
+Return $Crust.Metadata.ScriptResultCode
--- modules/crust.psm1
+++ modules/crust.psm1
@@ -1,32 +1,32 @@
 function Initialize-Crust {
   # Configs
-  New-Variable -Name "Config_initialize" -Value (Get-Content -Path ".\configs\crust.json" -Raw | ConvertFrom-Json) -Scope Global -Force
+  New-Variable -Name "Crust" -Value (Get-Content -Path ".\configs\crust.json" -Raw | ConvertFrom-Json) -Scope Global -Force
 
   # Metadata
-  $Global:Config_initialize.Metadata.StartDateTime = (Get-Date)
-  $Config_initialize.Metadata.CompleteDateTime = $null
-  $Config_initialize.Metadata.CompleteTimeSpan = $null
-  $Config_initialize.Metadata.ExecutionUser = $([System.Security.Principal.WindowsIdentity]::GetCurrent())
+  $Global:Crust.Metadata.StartDateTime = (Get-Date)
+  $Crust.Metadata.CompleteDateTime = $null
+  $Crust.Metadata.CompleteTimeSpan = $null
+  $Crust.Metadata.ExecutionUser = $([System.Security.Principal.WindowsIdentity]::GetCurrent())
 
   # Preferences
   $ErrorActionPreference = "Stop"
-  foreach ($Item in $Config_initialize.Preferences.PSObject.Properties) {
+  foreach ($Item in $Crust.Preferences.PSObject.Properties) {
     Set-Variable -Name $Item.Name -Value $Item.Value -Scope Global -Force
   }
 
   # Logging
   $Global:Params_Logging = @{
-    FilePath = "Filesystem::$($Config_initialize.Logging.Directory)$(Get-Date -Format $Config_initialize.Logging.TimestampFormat)$($Config_initialize.Logging.Filename)"
+    FilePath = "Filesystem::$($Crust.Logging.Directory)$(Get-Date -Format $Crust.Logging.TimestampFormat)$($Crust.Logging.Filename)"
     Append   = $true
   }
-  New-Item -Path $Config_initialize.Logging.Directory -ItemType Directory -Force | Out-Null
+  New-Item -Path $Crust.Logging.Directory -ItemType Directory -Force | Out-Null
 
   Out-File -InputObject " " @Params_Logging
   Out-File -InputObject "------------------------------------------------------------------------------" @Params_Logging
-  Out-File -InputObject "  $($Config_initialize.Application.Name)" @Params_Logging
+  Out-File -InputObject "  $($Crust.Application.Name)" @Params_Logging
   Out-File -InputObject "------------------------------------------------------------------------------" @Params_Logging
-  foreach ($Item in ($Config_initialize.Application.PSObject.Properties | Where-Object -Property "Name" -ne "Name")) {
-    Out-File -InputObject "  $($Item.Name.PadRight(($Config_initialize.Application.PSObject.Properties.Name | Measure-Object -Property Length -Maximum).Maximum + 1)): $($Item.Value)" @Params_Logging
+  foreach ($Item in ($Crust.Application.PSObject.Properties | Where-Object -Property "Name" -ne "Name")) {
+    Out-File -InputObject "  $($Item.Name.PadRight(($Crust.Application.PSObject.Properties.Name | Measure-Object -Property Length -Maximum).Maximum + 1)): $($Item.Value)" @Params_Logging
   }
   Out-File -InputObject "------------------------------------------------------------------------------" @Params_Logging
   Out-File -InputObject " " @Params_Logging
@@ -38,38 +38,11 @@ function Initialize-Crust {
     Out-File -InputObject "        $($Item.Key.PadRight(($PSBoundParameters.Keys | Measure-Object -Property Length -Maximum).Maximum + 1)): $($Item.Value)" @Params_Logging
   }
 
-  # Import Configs
-  Out-File -InputObject "    - Configs" @Params_Logging
-
-  try {
-    if (Test-Path -Path $Config_initialize.Paths.Configs) {
-      Get-ChildItem -Path $Config_initialize.Paths.Configs -Filter "*.json" | Where-Object -Property "Name" -notin "crust.json" | ForEach-Object {
-        $Temp_Name = $_.BaseName
-        $Temp_VarName = "Config_$($Temp_Name)"
-        $Temp_Path = $_.FullName
-        $Temp_Content = Get-Content -Path $Temp_Path -Raw | ConvertFrom-Json
-        New-Variable -Name $Temp_VarName -Value $Temp_Content -Scope Global -Force
-        Out-File -InputObject "        $($Temp_Name)" @Params_Logging
-        Out-File -InputObject "          Path: $($Temp_Path)" @Params_Logging
-        Out-File -InputObject "          Variable Name: $($Temp_VarName)" @Params_Logging
-      }
-      Remove-Variable -Name "Temp_*" -Force
-    }
-    else {
-      Out-File -InputObject "          Status: Failure - $($Error.Exception.Message)" @Params_Logging
-      Throw "Failure - $($Error.Exception.Message)"
-    }
-  }
-  catch {
-    Out-File -InputObject "          Status: Failure - $($Error.Exception.Message)" @Params_Logging
-    Throw "Failure - $($Error.Exception.Message)"
-  }
-
   # Import Modules
   Out-File -InputObject "    - Modules" @Params_Logging
 
-  if (Test-Path -Path $Config_initialize.Paths.Modules) {
-    foreach ($Item in (Get-ChildItem -Path $Config_initialize.Paths.Modules -Recurse -Filter "*.psm1" | Where-Object -Property "Name" -ne "crust.psm1")) {
+  if (Test-Path -Path $Crust.Paths.Modules) {
+    foreach ($Item in (Get-ChildItem -Path $Crust.Paths.Modules -Recurse -Filter "*.psm1" | Where-Object -Property "Name" -ne "crust.psm1")) {
       try {
         Out-File -InputObject "        $($Item.Name)" @Params_Logging
         Out-File -InputObject "          Path: $($Item.FullName)" @Params_Logging
@@ -98,7 +71,7 @@ function Initialize-Crust {
 
 function Set-Tokens {
   # Configs
-  New-Variable -Name "Language" -Value (Get-Content -Path "$($Config_initialize.Paths.Languages)$($UICulture)\$($UICulture).json" -Raw | ConvertFrom-Json) -Scope Global -Force
+  New-Variable -Name "Language" -Value (Get-Content -Path "$($Crust.Paths.Languages)$($UICulture)\$($UICulture).json" -Raw | ConvertFrom-Json) -Scope Global -Force
 
   # Update Dynamic Tokens
   foreach ($Item in $Language.DynamicToken.PSObject.Properties) {
@@ -179,7 +152,7 @@ function Write-Interface {
     [switch]  $LineBreakAfter,
     [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 5)]
     [ValidatePattern("[0,1000]")]
-    [string]  $SleepMilliseconds = $Config_initialize.Settings.InterfaceSleepMilliseconds
+    [string]  $SleepMilliseconds = $Crust.Settings.InterfaceSleepMilliseconds
   )
 
   begin {
@@ -263,17 +236,38 @@ function Get-ConfirmationToContinue {
   }
 }
 
+function Get-InterfaceMenu {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+    [string] $File
+  )
+
+  begin {
+
+  }
+
+  process {
+    # Get Object from Config File
+    $Temp_Object = Get-Content -Path "$($Crust.Paths.Configs)\$($File)" -Raw | ConvertFrom-Json
+  }
+
+  end {
+    # Return Object
+    Return $Temp_Object
+  }
+}
+
 function Show-InterfaceMenu {
   [CmdletBinding()]
   param (
     [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
-    [string] $MenuName
+    [object] $Menu,
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 1)]
+    [string] $Name
   )
 
   begin {
-    # Get Menu Object
-    $MenuObject = ($Language.MenuStructure | Where-Object -Property "Name" -eq $MenuName | Sort-Object -Property "Index")
-
     # Clear Interface
     Clear-Interface
     Write-Interface -Message $Language.Component.LineBreak -IndentLevel 0
@@ -282,24 +276,24 @@ function Show-InterfaceMenu {
     Initialize-Interface
 
     # Write- Message
-    Write-Interface -Message $MenuObject.Name -IndentLevel 1
+    Write-Interface -Message $Menu.Name -IndentLevel 1
 
     # Add White Space
     Write-Interface -Message $Interface.LineBreak -IndentLevel 0
 
     # Set incremental integer for ensuring only one divider line is written to the screen
-    $DividerLine_Increment = 0
+    $Divider_Increment = 0
   }
 
   process {
-    foreach ($MenuItem in $MenuObject.MenuItems) {
+    foreach ($Item in $Menu.MenuItems) {
       # Write Divider
-      if ($MenuItem.Index -match '^[a-zA-Z]*$') {
+      if ($Item.Index -match '^[a-zA-Z]*$') {
         # Increase divider increment
-        $DividerLine_Increment = $DividerLine_Increment + 1
+        $Divider_Increment = $Divider_Increment + 1
 
         # Add White Space
-        if ($DividerLine_Increment -lt 2) {
+        if ($Divider_Increment -lt 2) {
           Write-Interface -Message $Language.Component.LineBreak -IndentLevel 0
           Write-Interface -Message "-------------------------------------------------------" -IndentLevel 1
           Write-Interface -Message $Language.Component.LineBreak -IndentLevel 0
@@ -307,15 +301,15 @@ function Show-InterfaceMenu {
       }
 
       # Write Menu Items
-      Write-Interface -Message $MenuItem.Index -IndentLevel 2 -NoNewLine
-      Write-Interface -Message $MenuItem.Label -IndentLevel 2
+      Write-Interface -Message $Item.Index -IndentLevel 2 -NoNewLine
+      Write-Interface -Message $Item.Label -IndentLevel 2
     }
   }
 
   end {
     # Get Menu Choice and Return to Main Loop
-    $Menu_Choice = Get-InterfaceMenuInput -MenuObject $MenuObject
-    Return $Menu_Choice
+    $Result = Get-InterfaceMenuInput -Menu $Menu
+    Return $Result
   }
 }
 
@@ -323,7 +317,7 @@ function Get-InterfaceMenuInput {
   [CmdletBinding()]
   param (
     [Parameter(Mandatory = $true, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $false, Position = 0)]
-    [object] $MenuObject
+    [object] $Menu
   )
 
   begin {
@@ -331,16 +325,16 @@ function Get-InterfaceMenuInput {
     Write-Interface -Message $Language.Component.LineBreak -IndentLevel 0
     Write-Interface -Message $Language.Component.LineBreak -IndentLevel 0
     do { $Input_User = Read-Host -Prompt "Choose: " }
-    until (($Input_User -ne "") -and ($Input_User -in $MenuObject.MenuItems.Index))
+    until (($Input_User -ne "") -and ($Input_User -in $Menu.MenuItems.Index))
   }
 
   process {
     # Execute Menu Item Scriptblock
-    if (($MenuObject.MenuItems | Where-Object -Property "Index" -eq $Input_User).ScriptBlock -in "", $null, " ") {
-      Return ($MenuObject.MenuItems | Where-Object -Property "Index" -eq $Input_User).Label
+    if (($Menu.MenuItems | Where-Object -Property "Index" -eq $Input_User).ScriptBlock -in "", $null, " ") {
+      Return ($Menu.MenuItems | Where-Object -Property "Index" -eq $Input_User).Label
     }
     else {
-      Invoke-Command -ScriptBlock $([ScriptBlock]::Create(($MenuObject.MenuItems | Where-Object -Property "Index" -eq $Input_User).ScriptBlock))
+      Invoke-Command -ScriptBlock $([ScriptBlock]::Create(($Menu.MenuItems | Where-Object -Property "Index" -eq $Input_User).ScriptBlock))
     }
   }
 
